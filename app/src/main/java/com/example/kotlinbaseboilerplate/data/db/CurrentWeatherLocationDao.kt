e91b80ad2d0cf4a6b8f57740de6ee244fb552e7a
serranocjm <serrano.cjm@gmail.com>
2019-10-01 16:08:32 -0400

    Misc - Add DAOs for DB transactions - Class refactor

---
+++ app/src/main/java/com/example/kotlinbaseboilerplate/data/db/CurrentWeatherLocationDao.kt
@@ -0,0 +1,20 @@
+package com.example.kotlinbaseboilerplate.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.kotlinbaseboilerplate.data.db.entity.CURRENT_LOCATION_ID
+import com.example.kotlinbaseboilerplate.data.db.entity.CurrentWeatherLocation
+
+@Dao
+interface CurrentWeatherLocationDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun upsert(currentWeatherLocation: CurrentWeatherLocation)
+
+    @Query("select * from current_weather where id = $CURRENT_LOCATION_ID")
+    fun getCurrentLocation() : LiveData<CurrentWeatherLocation>
+
+}
--- app/src/main/java/com/example/kotlinbaseboilerplate/data/network/response/CurrentWeatherResponse.kt
+++ app/src/main/java/com/example/kotlinbaseboilerplate/data/network/response/CurrentWeatherResponse.kt
@@ -1,14 +1,14 @@
 package com.example.kotlinbaseboilerplate.data.network.response
 
 import com.example.kotlinbaseboilerplate.data.db.entity.CurrentWeatherEntry
-import com.example.kotlinbaseboilerplate.data.db.entity.Location
+import com.example.kotlinbaseboilerplate.data.db.entity.CurrentWeatherLocation
 import com.google.gson.annotations.SerializedName
 
 data class CurrentWeatherResponse(
     @SerializedName("current")
     val currentWeatherEntry: CurrentWeatherEntry,
     @SerializedName("location")
-    val location: Location,
+    val currentWeatherLocation: CurrentWeatherLocation,
     @SerializedName("request")
     val request: Request
 )
