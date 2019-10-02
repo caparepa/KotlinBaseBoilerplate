a8964fac11651257218ced88ba99f32be1d84817
serranocjm <serrano.cjm@gmail.com>
2019-10-02 14:53:50 -0400

    WIP: - Add extensions - Set initial layout - Adjust fragment class

--- app/src/main/java/com/example/kotlinbaseboilerplate/ui/weather/current/CurrentWeatherFragment.kt
+++ app/src/main/java/com/example/kotlinbaseboilerplate/ui/weather/current/CurrentWeatherFragment.kt
@@ -2,24 +2,20 @@ package com.example.kotlinbaseboilerplate.ui.weather.current
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 
 import com.example.kotlinbaseboilerplate.R
-import com.example.kotlinbaseboilerplate.data.WeatherStackApiService
-import com.example.kotlinbaseboilerplate.data.network.ConnectivityInterceptorImpl
-import com.example.kotlinbaseboilerplate.data.network.WeatherNetworkDataSourceImpl
 import com.example.kotlinbaseboilerplate.ui.base.ScopedFragment
+import com.example.kotlinbaseboilerplate.utils.makeGone
 import kotlinx.android.synthetic.main.current_weather_fragment.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.closestKodein
 import org.kodein.di.generic.instance
+
 
 /**
  * We make the fragment a subclass of scoped fragment so we can use the coroutine scope safely
@@ -59,7 +55,9 @@ class CurrentWeatherFragment : ScopedFragment(), KodeinAware {
         //ui interaction
         currentWeather.observe(this@CurrentWeatherFragment, Observer{
             if(it == null) return@Observer //if there is no data, return the observer until there is data!
-            //text_view.text = it.toString()
+
+            //Hide the loading group
+            group_loading.makeGone()
         })
     }
 
