bc973adc2078af1fd93593efc068791768a74c6a
serranocjm <serrano.cjm@gmail.com>
2019-10-03 10:26:55 -0400

    Glide implementation

---
+++ app/src/main/java/com/example/kotlinbaseboilerplate/internal/glide/ForecastAppGlideModule.kt
@@ -0,0 +1,8 @@
+package com.example.kotlinbaseboilerplate.internal.glide
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class ForecastAppGlideModule : AppGlideModule() {
+}
