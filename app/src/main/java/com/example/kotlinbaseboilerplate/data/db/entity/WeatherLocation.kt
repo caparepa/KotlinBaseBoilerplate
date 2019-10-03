e6903384ecabea1978137d4ed07069b9cc18ead0
serranocjm <serrano.cjm@gmail.com>
2019-10-03 14:10:20 -0400

    Weather location entity adjustments

--- app/src/main/java/com/example/kotlinbaseboilerplate/data/db/entity/WeatherLocation.kt
+++ app/src/main/java/com/example/kotlinbaseboilerplate/data/db/entity/WeatherLocation.kt
@@ -3,6 +3,9 @@ package com.example.kotlinbaseboilerplate.data.db.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
+import org.threeten.bp.Instant
+import org.threeten.bp.ZoneId
+import org.threeten.bp.ZonedDateTime
 
 const val WEATHER_LOCATION_ID = 0
 
@@ -31,4 +34,12 @@ data class WeatherLocation(
     // we'll set the primary key to false
     @PrimaryKey(autoGenerate = false)
     var id: Int = WEATHER_LOCATION_ID
+
+    //Helper property for date time
+    val zonedDateTime: ZonedDateTime
+        get() {
+            val instant = Instant.ofEpochSecond(localtimeEpoch)
+            val zoneId = ZoneId.of(timezoneId)
+            return ZonedDateTime.ofInstant(instant, zoneId)
+        }
 }
