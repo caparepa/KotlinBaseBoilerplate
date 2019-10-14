98087eaa2557a0b112e41e04ef032619256e407e
serranocjm <serrano.cjm@gmail.com>
2019-10-14 16:40:08 -0400

    Database entity (WIP)

--- app/src/main/java/com/example/kotlinbaseboilerplate/data/db/weatherbit/CurrentWeatherDataDao.kt
+++ app/src/main/java/com/example/kotlinbaseboilerplate/data/db/weatherbit/CurrentWeatherDataDao.kt
@@ -1,25 +1,23 @@
-package com.example.kotlinbaseboilerplate.data.db.weatherstack
+package com.example.kotlinbaseboilerplate.data.db.weatherbit
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.example.kotlinbaseboilerplate.data.db.weatherstack.entity.CURRENT_WEATHER_ID
-import com.example.kotlinbaseboilerplate.data.db.weatherstack.entity.CurrentWeatherEntry
+import com.example.kotlinbaseboilerplate.data.db.weatherbit.entity.BIT_CURRENT_DATA_ID
+import com.example.kotlinbaseboilerplate.data.db.weatherbit.entity.CurrentWeatherData
 
 @Dao
-interface CurrentWeatherEntryDao {
-
+interface CurrentWeatherDataDao {
     //We create an "upsert" function (update or insert) for the current weather
     //The onConflict strategy is used for the upsert (if there is a record, replace it)
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun upsert(weatherEntry: CurrentWeatherEntry)
+    fun upsert(weatherEntry: CurrentWeatherData)
 
     //In the tutorial, there are 2 interfaces that wrap the CurrentWeatherEntity
     //in order to get the units (imperial or metric). Since the API changed, we'll use
     //the original Entity
-    @Query("select * from current_weather_data where id = $CURRENT_WEATHER_ID")
-    fun getCurrentWeather() : LiveData<CurrentWeatherEntry>
-
+    @Query("select * from current_weather where id = $BIT_CURRENT_DATA_ID")
+    fun getCurrentWeather() : LiveData<CurrentWeatherData>
 }
