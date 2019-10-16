8070f28ba12cf3d32ed8a885f6bce0037f2a4a0a
serranocjm <serrano.cjm@gmail.com>
2019-10-16 14:55:17 -0400

    Database (WIP)

--- app/src/main/java/com/example/kotlinbaseboilerplate/data/db/weatherbit/BitWeatherDatabase.kt
+++ app/src/main/java/com/example/kotlinbaseboilerplate/data/db/weatherbit/BitWeatherDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.kotlinbaseboilerplate.data.db.weatherbit
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.kotlinbaseboilerplate.data.db.weatherbit.entity.CurrentWeatherData
 import com.example.kotlinbaseboilerplate.data.db.weatherbit.entity.WeatherDescription
@@ -10,4 +12,45 @@ import com.example.kotlinbaseboilerplate.data.db.weatherbit.entity.WeatherDescri
     version = 1
 )
 abstract class BitWeatherDatabase : RoomDatabase() {
+
+    abstract fun getCurrentWeatherDataDao() : CurrentWeatherDataDao
+    abstract fun getWeatherDescriptionDao() : WeatherDescriptionDao
+
+    //We create a companion object that will act as singleton in order to create a single instance
+    //of the database
+    companion object {
+
+        //@Volatile annotation allows all of the threads to have access to the property (instance)
+        @Volatile
+        private var instance: BitWeatherDatabase? = null
+
+        //Lock object for the threads
+        private val LOCK = Any()
+
+        //We create an operator function to initialize the database
+        //If there is an instance, return it (instance ?:)
+        //else, syncronize a block with a lock, check again if there is an instance,
+
+        operator fun invoke(context: Context) = instance
+            ?: synchronized(LOCK) {
+                //if there is no instance, build a database (buildDatabase(context))
+                //and also, whatever is returned from the builder, set the instance equal to "it"
+                instance
+                    ?: buildDatabase(
+                        context
+                    ).also {
+                        instance = it
+                    }
+            }
+
+        //We create a function to build the database, which will receive the application context
+        ///This builder will initialize the database java class for the app (ForecastDatabase)
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                BitWeatherDatabase::class.java, "forecast.db"
+            ).build()
+
+    }
+
 }
