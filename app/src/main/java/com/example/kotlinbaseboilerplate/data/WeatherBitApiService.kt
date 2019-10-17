784fa767cd0f34cf3dfa88b6f05cdb2c04108c6d
serranocjm <serrano.cjm@gmail.com>
2019-10-17 11:13:57 -0400

    API service interface (WIP)

---
+++ app/src/main/java/com/example/kotlinbaseboilerplate/data/WeatherBitApiService.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinbaseboilerplate.data
+
+import kotlinx.coroutines.Deferred
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+const val BIT_API_KEY = ""
+const val BIT_BASE_URL = ""
+
+interface WeatherBitApiService {
+}
