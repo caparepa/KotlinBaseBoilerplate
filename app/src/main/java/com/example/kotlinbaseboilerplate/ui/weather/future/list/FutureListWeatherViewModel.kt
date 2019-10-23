7f6385c268d5cd831ad5938fbb1e3b9684795185
serranocjm <serrano.cjm@gmail.com>
2019-10-23 14:49:41 -0400

    ViewModel setup

--- app/src/main/java/com/example/kotlinbaseboilerplate/ui/weather/future/list/FutureListWeatherViewModel.kt
+++ app/src/main/java/com/example/kotlinbaseboilerplate/ui/weather/future/list/FutureListWeatherViewModel.kt
@@ -1,10 +1,20 @@
 package com.example.kotlinbaseboilerplate.ui.weather.future.list
 
 import androidx.lifecycle.ViewModel
+import com.example.kotlinbaseboilerplate.data.provider.UnitProvider
 import com.example.kotlinbaseboilerplate.data.repository.ForecastRepository
+import com.example.kotlinbaseboilerplate.internal.UnitSystem
+import com.example.kotlinbaseboilerplate.internal.lazyDeferred
+import com.example.kotlinbaseboilerplate.ui.base.WeatherViewModel
+import org.threeten.bp.LocalDate
 
 class FutureListWeatherViewModel(
-    private val forecastRepository: ForecastRepository
-) : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val forecastRepository: ForecastRepository,
+    unitProvider: UnitProvider
+) : WeatherViewModel(forecastRepository, unitProvider) {
+
+    val weatherEntries by lazyDeferred {
+        //forecastRepository.getFutureWeatherList(LocalDate.now(), super.isMetricUnit)
+        forecastRepository.getFutureWeatherList(LocalDate.now())
+    }
 }
