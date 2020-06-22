963e12df6e129107f35e7d6b83386298ce463d8c
C. Serrano <serrano.cjm@gmail.com>
2020-06-22 12:51:04 -0400

    Add repository

--- app/src/main/java/com/example/kotlinbaseboilerplate/data/repository/refactor/ForecastRepository.kt
+++ app/src/main/java/com/example/kotlinbaseboilerplate/data/repository/refactor/ForecastRepository.kt
@@ -1,6 +1,14 @@
 package com.example.kotlinbaseboilerplate.data.repository.refactor
 
+import com.example.kotlinbaseboilerplate.data.WeatherBitApiService
+import com.example.kotlinbaseboilerplate.data.db.weatherbit.WeatherDatabase
+import com.example.kotlinbaseboilerplate.data.network.refactor.SafeApiRequest
+import com.example.kotlinbaseboilerplate.data.provider.PreferenceProvider
 import com.example.kotlinbaseboilerplate.data.repository.BaseRepository
 
-class ForecastRepository: BaseRepository() {
+class ForecastRepository(
+    private val wbApi: WeatherBitApiService,
+    private val wbDb: WeatherDatabase,
+    private val wbPrefs: PreferenceProvider
+) : SafeApiRequest() {
 }
